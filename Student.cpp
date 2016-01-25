eb621a96e7980002eb7154bf37a541cc5a5b8522
GrigoryanArut <grigoryan.as1305@asugubkin.ru>
2016-01-25 06:21:31 +0300

    Create Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,22 @@
+#include "Student.h"
+
+
+void Student::funceasy()
+{
+	cout << "Easy function of the base class" << endl;
+}
+
+void Student::funcvirtual()
+{
+	cout << "Virtual function of the base class" << endl;
+
+}
+
+Student::Student()
+{
+}
+
+
+Student::~Student()
+{
+}
