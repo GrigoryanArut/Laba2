55e1ba93ff137ae672d66733a2f3b5160a4846d0
GrigoryanArut <grigoryan.as1305@asugubkin.ru>
2016-01-25 06:21:54 +0300

    Create Groups.cpp

---
+++ Groups.cpp
@@ -0,0 +1,25 @@
+#include "Groups.h"
+
+
+void Groups::funceasy()
+{
+	cout << "Easy function of the derived class" << endl;
+}
+
+void Groups::funcvirtual()
+{
+	cout << "Virtual function of the derived class" << endl;
+
+}
+
+Groups::Groups()
+{
+}
+
+
+Groups::~Groups()
+{
+}
+
+
+
